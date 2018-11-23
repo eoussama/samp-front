99910d9dc88e0906e2fc661e2cd08153917580be
Eoussama <oussama_gtacj@live.fr>
2018-11-23 14:57:23 +0000

    index.php populated.

---
+++ src/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="EOussama">
+        <meta name="application-name" content="project test">
+        <meta name="description" content="dfqfd">
+        <meta name="keywords" content="fsgfsd">
+
+        <link rel="stylesheet" type="text/css" href="assets/fonts/font-awesome/css/fontawesome-all.min.css">
+        
+        
+        <link rel="shortcut icon" type="image/png" href="assets/images/">
+        <link rel="stylesheet" type="text/css" href="assets/styles/main.css">
+
+        <title>project test</title>
+    </head>
+
+    <body>
+
+        
+        
+        
+        <script type="text/javascript" src="assets/scripts/main.js"></script>
+    </body>
+</html>
