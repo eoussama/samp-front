1b50ab82ff3133a48655b625796f40b26781a6ec
Eoussama <oussama_gtacj@live.fr>
2018-11-23 15:53:58 +0000

    Basic files added

--- src/index.php
+++ src/index.php
@@ -1,29 +1,21 @@
-<?php ?>
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="author" content="EOussama">
-        <meta name="application-name" content="project test">
-        <meta name="description" content="dfqfd">
-        <meta name="keywords" content="fsgfsd">
-
-        <link rel="stylesheet" type="text/css" href="assets/fonts/font-awesome/css/fontawesome-all.min.css">
+        <meta name="application-name" content="Samp Front">
+        <meta name="description" content="A starter landing page template for samp servers.">
+        <meta name="keywords" content="template, landing-page, samp, samp-server, samp-website">
         
-        
-        <link rel="shortcut icon" type="image/png" href="assets/images/">
-        <link rel="stylesheet" type="text/css" href="assets/styles/main.css">
+        <link rel="shortcut icon" type="image/png" href="assets/img/logo.png">
+        <link rel="stylesheet" href="assets/css/main.css">
 
         <title>project test</title>
     </head>
 
     <body>
 
-        
-        
-        
-        <script type="text/javascript" src="assets/scripts/main.js"></script>
+        <script src="assets/js/main.js"></script>
     </body>
 </html>
