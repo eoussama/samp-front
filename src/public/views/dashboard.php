267873ba696c7532d9d90d04abee585ffc19fd51
Eoussama <oussama_gtacj@live.fr>
2018-12-03 14:50:44 +0000

    HTML content type added.

--- src/public/views/dashboard.php
+++ src/public/views/dashboard.php
@@ -52,6 +52,7 @@
 
         <!-- The meta data. -->
         <meta charset="UTF-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="author" content="EOussama">
